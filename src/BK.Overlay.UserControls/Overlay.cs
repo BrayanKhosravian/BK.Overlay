c56d4897159fe53dcf445603ec946dd69a7efaa7
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-04-25 01:06:57 +0200

    its possible to add uielments to the overlay

--- src/BK.Overlay.UserControls/Overlay.cs
+++ src/BK.Overlay.UserControls/Overlay.cs
@@ -1,7 +1,16 @@
-﻿using BK.Overlay.UserControls.Common;
+﻿using BK.Overlay.Core.Shapes;
+using BK.Overlay.UserControls.Common;
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace BK.Overlay.UserControls
 {
+	[ContentProperty(nameof(CanvasItems))]
 	public class Overlay : CustomWindow
 	{
 		public Overlay()
@@ -9,9 +18,69 @@ namespace BK.Overlay.UserControls
 			var resources = new OverlayResources();
 			Resources = resources;
 
-			var dpiDecorator = new DpiDecorator();
-			
-
+			var root = CreateRoot();
+			Content = root; 
         }
+
+		#region ContentProperty
+
+		public ObservableCollection<UIElement> CanvasItems
+		{
+			get => (ObservableCollection<UIElement>)GetValue(CanvasItemsProperty);
+			set => SetValue(CanvasItemsProperty, value);
+		}
+
+		public static readonly DependencyProperty CanvasItemsProperty = DependencyProperty.Register(nameof(CanvasItems), 
+			typeof(ObservableCollection<UIElement>),
+			typeof(Overlay), 
+			new PropertyMetadata(new ObservableCollection<UIElement>(), new PropertyChangedCallback(CanvasItemsPropertyChanged)));
+
+		private static void CanvasItemsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var old = (ObservableCollection<UIElement>)e.OldValue;
+			if (old != null)
+			{
+				old.CollectionChanged -= CanvasItemsCollectionChanged;
+			}
+			((ObservableCollection<UIElement>)e.NewValue).CollectionChanged += CanvasItemsCollectionChanged;
+		}
+
+		private static void CanvasItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			
+		}
+
+		#endregion
+
+		private DpiDecorator CreateRoot()
+		{
+			
+			// canvas
+			var itemsControl = new ItemsControl();
+			DataContext = this;
+			
+			var canvasItemsBinding = new Binding(nameof(OverlayViewModelBase.CanvasItems));
+			itemsControl.SetBinding(ItemsControl.ItemsSourceProperty, canvasItemsBinding);
+			itemsControl.SetBinding(ItemsControl.ItemsSourceProperty, new Binding(nameof(Overlay.CanvasItems)));
+
+		//	itemsControl.ItemTemplateSelector = new CanvasItemSelector();
+			// itemsControl.ItemContainerStyleSelector = new CanvasItemContainerStyleSelector();
+
+			var itemsPanelTemplate = new ItemsPanelTemplate(new System.Windows.FrameworkElementFactory(typeof(Canvas)));
+			itemsControl.ItemsPanel = itemsPanelTemplate;
+
+			// dpi decorator
+
+			var dpiDecorator = new DpiDecorator();
+			dpiDecorator.Child = itemsControl;
+
+			return dpiDecorator;
+
+		}
     }
+
+	abstract class OverlayViewModelBase
+	{
+		public ObservableCollection<Shape> CanvasItems { get; protected set; } = new ObservableCollection<Shape>();
+	}
 }
