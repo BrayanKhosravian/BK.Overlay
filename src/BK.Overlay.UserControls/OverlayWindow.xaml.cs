00f1d0c9dc5262561f4dbcfb162acdb1d00af23b
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-04-25 11:31:45 +0200

    added overlay

--- src/BK.Overlay.UserControls/OverlayWindow.xaml.cs
+++ src/BK.Overlay.UserControls/OverlayWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,29 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using BK.Overlay.Example.ViewModels;
-using BK.Overlay.UserControls;
 
-namespace BK.Overlay.Example
+namespace BK.Overlay.UserControls
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for OverlayWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class OverlayWindow : Window
 	{
-		public MainWindow()
+		public OverlayWindow()
 		{
 			InitializeComponent();
-
-		
-		}
-
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-			
-
 		}
 	}
 }
--- src/BK.Overlay.UserControls/UserControl1.xaml.cs
+++ src/BK.Overlay.UserControls/UserControl1.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,27 +10,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using BK.Overlay.Example.ViewModels;
-using BK.Overlay.UserControls;
 
-namespace BK.Overlay.Example
+namespace BK.Overlay.UserControls
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for UserControl1.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class UserControl1 : UserControl
 	{
-		public MainWindow()
+		public UserControl1()
 		{
 			InitializeComponent();
-
-		
-		}
-
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-			
-
 		}
 	}
 }
