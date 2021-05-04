28ee1b55a2e1aafa2aa0a5a9ef31eb9bfc6a6c4b
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2021-05-04 20:35:10 +0200

    added cheat sheet overlay

---
+++ src/Projects/BK.CheatSheetOverlay/Views/CheatSheetOverlay.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BK.CheatSheetOverlay.Views
+{
+	/// <summary>
+	/// Interaction logic for CheatSheetOverlay.xaml
+	/// </summary>
+	public partial class CheatSheetOverlay : Window
+	{
+		public CheatSheetOverlay()
+		{
+			InitializeComponent();
+		}
+	}
+}
